909550e7150036342c9fb9296ab18382a1424dea
HannaShulman <hsc@cookieinformation.com>
2023-11-20 09:22:42 +0000

    Revert removal of initSDK, and update to 2.0.4.

--- library/src/main/java/com/cookieinformation/mobileconsents/MobileConsents.kt
+++ library/src/main/java/com/cookieinformation/mobileconsents/MobileConsents.kt
@@ -52,6 +52,12 @@ public class MobileConsents constructor(
     }
   }
 
+  public suspend fun initSDK() {
+    // For now the only thing we have to do here is to perform a migration from the version that introduced
+    // a problem consent types
+    mobileConsentSdk.init()
+  }
+
   /**
    * Returns associated instance of MobileConsentSdk.
    *
