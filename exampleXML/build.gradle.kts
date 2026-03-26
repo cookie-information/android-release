d6f4fac59714029f18e4daeedc1c429a2097d9c5
Karolina Poplawska <k.poplawska@piwik.pro>
2026-03-26 20:42:46 +0100

    [PPSDK-372] fix tests

--- exampleXML/build.gradle.kts
+++ exampleXML/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.example.examplexml"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.examplexml"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
